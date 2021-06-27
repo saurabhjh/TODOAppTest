39a3c7ac805289834abbe9db0a11f292c011ddb3
Saurabh Jha <saurabhjhajha5@gmail.com>
2021-06-28 00:30:29 +0530

    second commit

--- src/About.js
+++ src/About.js
@@ -2,7 +2,11 @@
 import "./About.css"
 export const About = () => {
     return (
-        <div className="about-design" >This is about section
-            <br />dsdjlds<br />skdjndsnosidnosndo<br /></div>
+        <div className="about-design" >
+            This is about section <br/>
+            I have designed app to test my knowledge on react<br/>
+            Share your view on the below Social Apps.
+            <br/>Thanks.
+        </div>
     )
 }
